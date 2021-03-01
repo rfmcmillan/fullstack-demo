95b979a9b8b2695d17ad3fa14bcb49824110a000
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-03-01 15:51:59 -0500

    separated out Users component

---
+++ client/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Users from './Components/Users';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({
+      users: (await axios.get('/api/users')).data,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { users, loading } = this.state;
+    if (loading) {
+      return '...loading';
+    }
+    return (
+      <div>
+        <Users users={users} />
+      </div>
+    );
+  }
+}
+
+export default App;
